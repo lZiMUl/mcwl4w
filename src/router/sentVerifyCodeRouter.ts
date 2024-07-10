a18ad983724ea6e30261f0f6a47fc3a85dc68c7d
lZiMUl <lzimul@lzimul.top>
2024-07-10 06:39:56 -0700

    Fixed and format

--- src/router/sentVerifyCodeRouter.ts
+++ src/router/sentVerifyCodeRouter.ts
@@ -9,12 +9,11 @@ const koaRouter: KoaRouter = new KoaRouter();
 koaRouter.post("/sentVerifyCode", async (socket: ParameterizedContext): Promise<void> => {
 	const target: string = (socket.request.body as DataModule).email;
 	const verifyCode: number = GenerateRandomCaptcha.generate((socket.request.body as DataModule).username);
-	console.info(target, verifyCode);
 	await emailService.service.sendMail({
 		from: emailService.config.user,
 		to: target,
-		subject: "Hello ✔",
-		text: "Hello world?",
+		subject: "服务器验证码",
+		text: verifyCode.toString(),
 		html: verifyCode.toString()
 	});
 });
