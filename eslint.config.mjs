8df428c468e90f06138ab85a0770b730e0c50224
lZiMUl <lzimul@lzimul.top>
2024-07-12 13:29:51 -0700

    Added ACU Model

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
-// import globals from "globals";
-// import tseslint from "typescript-eslint";
-//
-// export default [
-// 	{
-// 		files: ["src/**/*.ts"],
-// 		languageOptions: {
-// 			globals: globals.node
-// 		},
-// 		...tseslint.configs.recommended
-// 	}
-// ];
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+	{
+		files: ["src/**/*.ts"],
+		languageOptions: {
+			globals: globals.node
+		},
+		...tseslint.configs.recommended
+	}
+];
