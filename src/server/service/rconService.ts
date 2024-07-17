34c2c82a04291b9e32fac11adb54efeac8ceeac6
lZiMUl <lzimul@lzimul.top>
2024-07-17 01:59:29 -0700

    Format code

--- src/server/service/rconService.ts
+++ src/server/service/rconService.ts
@@ -5,9 +5,9 @@ import { ServiceType } from '../type/serviceType';
 const serviceType: ServiceType = 'rconService';
 
 const [host, port, password]: [string, number, string] = [
-  (getConfig(serviceType, 'host') ?? '127.0.0.1') as string,
-  (getConfig(serviceType, 'port') ?? 25575) as number,
-  (getConfig(serviceType, 'password') ?? '') as string
+  getConfig(serviceType, 'host') ?? '127.0.0.1',
+  getConfig(serviceType, 'port') ?? 25575,
+  getConfig(serviceType, 'password') ?? ''
 ];
 
 export default new Rcon({ host, port, password });
